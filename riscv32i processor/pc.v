17b04cbd93914b160d47179bc80e21bc1a4310ac
Aleksandar Savic <acasavic2006@gmail.com>
2026-08-12 01:30:04 +0200

    Dodata glavna sema procesora i RAM(privremeno) i ROM memorija

---
+++ riscv32i processor/pc.v
@@ -0,0 +1,69 @@
+// Copyright (C) 1991-2013 Altera Corporation
+// Your use of Altera Corporation's design tools, logic functions 
+// and other software and tools, and its AMPP partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Altera Program License 
+// Subscription Agreement, Altera MegaCore Function License 
+// Agreement, or other applicable license agreement, including, 
+// without limitation, that your use is for the sole purpose of 
+// programming logic devices manufactured by Altera and sold by 
+// Altera or its authorized distributors.  Please refer to the 
+// applicable agreement for further details.
+
+// PROGRAM		"Quartus II 64-Bit"
+// VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
+// CREATED		"Tue Aug 11 23:42:39 2026"
+
+module pc(
+	CLK,
+	RESET,
+	PC
+);
+
+
+input wire	CLK;
+input wire	RESET;
+output wire	[31:0] PC;
+
+wire	0;
+wire	1;
+wire	[31:0] PC_ALTERA_SYNTHESIZED;
+wire	[31:0] PCplus4;
+wire	SYNTHESIZED_WIRE_0;
+
+assign	SYNTHESIZED_WIRE_0 = 1;
+wire	[31:0] GDFX_TEMP_SIGNAL_0;
+
+
+assign	GDFX_TEMP_SIGNAL_0 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0};
+
+
+REG32_LD_CL_INC_DEC	b2v_inst(
+	.LD(SYNTHESIZED_WIRE_0),
+	.C0(0),
+	.INC(0),
+	.E0(0),
+	.DEC(0),
+	.CL(RESET),
+	.clk(CLK),
+	.I(PCplus4),
+	
+	
+	.A(PC_ALTERA_SYNTHESIZED));
+
+
+
+
+ADD32	b2v_inst8(
+	.C_0(0),
+	.A(PC_ALTERA_SYNTHESIZED),
+	.B(GDFX_TEMP_SIGNAL_0),
+	
+	.OUT(PCplus4));
+
+assign	PC = PC_ALTERA_SYNTHESIZED;
+assign	0 = 0;
+
+endmodule
