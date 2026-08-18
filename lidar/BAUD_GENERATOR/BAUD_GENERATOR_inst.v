4e8e738e04f03153df479fa6e792150460981144
Neske <agentrambo63@gmail.com>
2026-08-18 23:32:04 +0200

    added: UART RX modul, next stop -> parser

---
+++ lidar/BAUD_GENERATOR/BAUD_GENERATOR_inst.v
@@ -0,0 +1,6 @@
+BAUD_GENERATOR	BAUD_GENERATOR_inst (
+	.clock ( clock_sig ),
+	.cnt_en ( cnt_en_sig ),
+	.sclr ( sclr_sig ),
+	.q ( q_sig )
+	);
---
+++ lidar/lpm_counter/counter_8bit_inst.v
@@ -0,0 +1,6 @@
+counter_8bit	counter_8bit_inst (
+	.clock ( clock_sig ),
+	.cnt_en ( cnt_en_sig ),
+	.sclr ( sclr_sig ),
+	.q ( q_sig )
+	);
