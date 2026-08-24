a5fdb81fabdf963fd50136523fc5a3175f987fcd
Neske <agentrambo63@gmail.com>
2026-08-24 16:14:59 +0200

    refactored: riscv32i_4 as final version of the project, added comments and reorganized blocks in the .bdf file

--- lidar/packet_parser/PacketParser.v
+++ lidar/packet_parser/PacketParser.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
-// CREATED		"Sat Aug 22 23:21:14 2026"
+// CREATED		"Mon Aug 24 15:44:31 2026"
 
 module PacketParser(
 	clk,
