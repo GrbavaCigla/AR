6225c55071803588717bcd0ba3d6edc1c8893bdd
Neske <agentrambo63@gmail.com>
2026-08-27 00:59:01 +0200

    made: riscv32i_4 as a new block and added a new final scheme where we would connected cpu with bus, vga, lidar and sdram

--- riscv32i processor/finalnaVerzija/riscvi32i_4.v
+++ riscv32i processor/finalnaVerzija/riscvi32i_4.v
@@ -14,44 +14,28 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
-// CREATED		"Wed Aug 26 18:44:09 2026"
+// CREATED		"Thu Aug 27 00:49:58 2026"
 
 module riscvi32i_4(
 	CLK,
 	RESET,
-	rx,
-	N,
-	Z,
-	sdram_cke,
-	sdram_cs_n,
-	sdram_ras_n,
-	sdram_cas_n,
-	sdram_we_n,
-	sdram_dqml,
-	sdram_dqmh,
-	RESULT,
-	sdram_addr,
-	sdram_ba,
-	sdram_dq
+	bus_wait,
+	bus_rdata,
+	bus_wr,
+	bus_rd,
+	bus_addr,
+	bus_wdata
 );
 
 
 input wire	CLK;
 input wire	RESET;
-input wire	rx;
-output wire	N;
-output wire	Z;
-output wire	sdram_cke;
-output wire	sdram_cs_n;
-output wire	sdram_ras_n;
-output wire	sdram_cas_n;
-output wire	sdram_we_n;
-output wire	sdram_dqml;
-output wire	sdram_dqmh;
-output wire	[31:0] RESULT;
-output wire	[12:0] sdram_addr;
-output wire	[1:0] sdram_ba;
-output wire	[15:0] sdram_dq;
+input wire	bus_wait;
+input wire	[31:0] bus_rdata;
+output wire	bus_wr;
+output wire	bus_rd;
+output wire	[31:0] bus_addr;
+output wire	[31:0] bus_wdata;
 
 wire	[7:0] _01100111;
 wire	[31:0] A;
@@ -68,15 +52,7 @@ wire	bltu;
 wire	bne;
 wire	Branch;
 wire	branch_taken;
-wire	[31:0] bus_rdata;
-wire	bus_ready;
-wire	bus_wait;
 wire	[31:0] cetiri;
-wire	cs_lidar;
-wire	cs_sdram;
-wire	[31:0] d_rdata;
-wire	d_ready;
-wire	[31:0] dbus_sdram_rd;
 wire	[31:0] IMM_OUT;
 wire	[2:0] immSrc;
 wire	[31:0] INSTRUCTIONS;
@@ -88,6 +64,7 @@ wire	JUMP;
 wire	MemRead;
 wire	[1:0] MemToReg;
 wire	MemWrite;
+wire	N;
 wire	[31:0] NEXT_PC;
 wire	nula;
 wire	[31:0] nula32BITS;
@@ -97,42 +74,28 @@ wire	[31:0] PCplus4;
 wire	[31:0] PCplusIMM;
 wire	realRegWrite;
 wire	RegWrite;
-wire	[31:0] RESULT_ALTERA_SYNTHESIZED;
+wire	[31:0] RESULT;
 wire	[31:0] RS1DATA;
 wire	[31:0] RS2DATA;
-wire	[25:0] sdram_abus;
-wire	[3:0] sdram_ben;
-wire	[31:0] sdram_dbus_wr;
-wire	sdram_read;
-wire	sdram_ready;
-wire	sdram_write;
 wire	sys_reset;
-wire	[31:0] v_rdata;
-wire	v_ready;
-wire	Z_ALTERA_SYNTHESIZED;
-wire	SYNTHESIZED_WIRE_0;
-wire	[31:0] SYNTHESIZED_WIRE_1;
-wire	[31:0] SYNTHESIZED_WIRE_2;
+wire	Z;
+wire	[31:0] SYNTHESIZED_WIRE_0;
+wire	SYNTHESIZED_WIRE_1;
+wire	SYNTHESIZED_WIRE_2;
 wire	SYNTHESIZED_WIRE_3;
 wire	SYNTHESIZED_WIRE_4;
-wire	SYNTHESIZED_WIRE_5;
+wire	[31:0] SYNTHESIZED_WIRE_5;
 wire	SYNTHESIZED_WIRE_6;
-wire	[31:0] SYNTHESIZED_WIRE_7;
-wire	SYNTHESIZED_WIRE_8;
-wire	SYNTHESIZED_WIRE_9;
-wire	SYNTHESIZED_WIRE_10;
 
-wire	[3:0] GDFX_TEMP_SIGNAL_4;
 wire	[31:0] GDFX_TEMP_SIGNAL_0;
-wire	[7:0] GDFX_TEMP_SIGNAL_5;
+wire	[7:0] GDFX_TEMP_SIGNAL_4;
 wire	[31:0] GDFX_TEMP_SIGNAL_3;
 wire	[31:0] GDFX_TEMP_SIGNAL_2;
 wire	[7:0] GDFX_TEMP_SIGNAL_1;
 
 
-assign	GDFX_TEMP_SIGNAL_4 = {jedan,jedan,jedan,jedan};
 assign	GDFX_TEMP_SIGNAL_0 = {JALR_SUM[31:1],nula};
-assign	GDFX_TEMP_SIGNAL_5 = {nula,INSTRUCTIONS[6:0]};
+assign	GDFX_TEMP_SIGNAL_4 = {nula,INSTRUCTIONS[6:0]};
 assign	GDFX_TEMP_SIGNAL_3 = {nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula};
 assign	GDFX_TEMP_SIGNAL_2 = {nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,nula,jedan,nula,nula};
 assign	GDFX_TEMP_SIGNAL_1 = {nula,jedan,jedan,nula,nula,jedan,jedan,jedan};
@@ -152,20 +115,6 @@ ImmediateGenerator	b2v_inst1(
 	.IMM_OUT(IMM_OUT));
 
 
-BusDecoder	b2v_inst10(
-	.bus_wr(MemWrite),
-	.bus_rd(MemRead),
-	.sdram_ready(sdram_ready),
-	.lidar_ready(SYNTHESIZED_WIRE_0),
-	.bus_addr(RESULT_ALTERA_SYNTHESIZED),
-	.lidar_rdata(SYNTHESIZED_WIRE_1),
-	.sdram_rdata(dbus_sdram_rd),
-	.cs_sdram(cs_sdram),
-	.cs_lidar(cs_lidar),
-	.bus_ready(bus_ready),
-	.bus_rdata(bus_rdata));
-
-
 ADD32	b2v_inst11(
 	.C_0(nula),
 	.A(PC),
@@ -175,18 +124,18 @@ ADD32	b2v_inst11(
 
 
 MPX4_32BIT	b2v_inst12(
-	.data0x(RESULT_ALTERA_SYNTHESIZED),
+	.data0x(RESULT),
 	.data1x(bus_rdata),
 	.data2x(PCplus4),
 	.data3x(nula32BITS),
 	.sel(MemToReg),
-	.result(SYNTHESIZED_WIRE_7));
+	.result(SYNTHESIZED_WIRE_5));
 
 
 pc	b2v_inst13(
 	.RESET(sys_reset),
 	.CLK(CLK),
-	.NEXT_PC(SYNTHESIZED_WIRE_2),
+	.NEXT_PC(SYNTHESIZED_WIRE_0),
 	.PC(PC));
 
 
@@ -198,16 +147,6 @@ ADD32	b2v_inst14(
 	.OUT(JALR_SUM));
 
 
-LidarBusInterface	b2v_inst15(
-	.clk(CLK),
-	.rx(rx),
-	.rst_n(RESET),
-	.cs_lidar(cs_lidar),
-	.bus_addr(RESULT_ALTERA_SYNTHESIZED),
-	.lidar_ready(SYNTHESIZED_WIRE_0),
-	.dbus_lidar_rd(SYNTHESIZED_WIRE_1));
-
-
 MPX4_32BIT	b2v_inst16(
 	.data0x(PCplus4),
 	.data1x(PCplusIMM),
@@ -216,9 +155,9 @@ MPX4_32BIT	b2v_inst16(
 	.sel(pc_sel),
 	.result(NEXT_PC));
 
-assign	SYNTHESIZED_WIRE_3 = beq | blt | bne | bge | bltu | bgeu;
+assign	SYNTHESIZED_WIRE_1 = beq | blt | bne | bge | bltu | bgeu;
 
-assign	branch_taken = SYNTHESIZED_WIRE_3 & Branch;
+assign	branch_taken = SYNTHESIZED_WIRE_1 & Branch;
 
 
 ControlWord	b2v_inst19(
@@ -239,22 +178,22 @@ ALU	b2v_inst2(
 	.A(A),
 	.ALUControl(ALUControl),
 	.B(B),
-	.N(N),
-	.Z(Z_ALTERA_SYNTHESIZED),
-	.RESULT(RESULT_ALTERA_SYNTHESIZED));
+	
+	.Z(Z),
+	.RESULT(RESULT));
 
 assign	pc_sel[1] = is_JALR;
 
 
-assign	SYNTHESIZED_WIRE_5 =  ~is_JALR;
+assign	SYNTHESIZED_WIRE_3 =  ~is_JALR;
 
-assign	is_JAL = JUMP & SYNTHESIZED_WIRE_4;
+assign	is_JAL = JUMP & SYNTHESIZED_WIRE_2;
 
-assign	SYNTHESIZED_WIRE_4 =  ~is_JALR;
+assign	SYNTHESIZED_WIRE_2 =  ~is_JALR;
 
-assign	pc_sel[0] = SYNTHESIZED_WIRE_5 & SYNTHESIZED_WIRE_6;
+assign	pc_sel[0] = SYNTHESIZED_WIRE_3 & SYNTHESIZED_WIRE_4;
 
-assign	SYNTHESIZED_WIRE_6 = branch_taken | is_JAL;
+assign	SYNTHESIZED_WIRE_4 = branch_taken | is_JAL;
 
 assign	_01100111 = GDFX_TEMP_SIGNAL_1;
 
@@ -273,32 +212,9 @@ REGFILE	b2v_inst3(
 	.RD(INSTRUCTIONS[11:7]),
 	.RS1(INSTRUCTIONS[19:15]),
 	.RS2(INSTRUCTIONS[24:20]),
-	.WRITEDATA(SYNTHESIZED_WIRE_7),
+	.WRITEDATA(SYNTHESIZED_WIRE_5),
 	.RS1DATA(RS1DATA),
 	.RS2DATA(RS2DATA));
-
-
-sdram_arbiter	b2v_inst30(
-	.d_write(SYNTHESIZED_WIRE_8),
-	.d_read(SYNTHESIZED_WIRE_9),
-	.v_read(nula),
-	.CLK(CLK),
-	.rst_n(RESET),
-	.sdram_ready(sdram_ready),
-	.d_addr(RESULT_ALTERA_SYNTHESIZED[25:0]),
-	.d_ben(GDFX_TEMP_SIGNAL_4),
-	.d_wdata(RS2DATA),
-	.sdram_dbus_rd(dbus_sdram_rd),
-	.v_addr(RESULT_ALTERA_SYNTHESIZED[25:0]),
-	.sdram_write(sdram_write),
-	.sdram_read(sdram_read),
-	
-	
-	
-	.sdram_abus(sdram_abus),
-	.sdram_ben(sdram_ben),
-	.sdram_dbus_wr(sdram_dbus_wr)
-	);
 
 
 MPX2_32BIT	b2v_inst31(
@@ -306,22 +222,11 @@ MPX2_32BIT	b2v_inst31(
 	.E(jedan),
 	.I0(NEXT_PC),
 	.I1(PC),
-	.D(SYNTHESIZED_WIRE_2));
+	.D(SYNTHESIZED_WIRE_0));
 
-assign	realRegWrite = RegWrite & SYNTHESIZED_WIRE_10;
+assign	realRegWrite = RegWrite & SYNTHESIZED_WIRE_6;
 
-assign	SYNTHESIZED_WIRE_10 =  ~bus_wait;
-
-
-BusFSM2	b2v_inst35(
-	.mem_read(MemRead),
-	.mem_write(MemWrite),
-	.bus_ready(bus_ready),
-	.bus_wait(bus_wait));
-
-assign	SYNTHESIZED_WIRE_9 = cs_sdram & MemRead;
-
-assign	SYNTHESIZED_WIRE_8 = cs_sdram & MemWrite;
+assign	SYNTHESIZED_WIRE_6 =  ~bus_wait;
 
 
 InstructionMemory	b2v_inst39(
@@ -339,37 +244,13 @@ MPX2_32BIT	b2v_inst4(
 	.I1(IMM_OUT),
 	.D(B));
 
-
-sdram_wrapper	b2v_inst41(
-	.clk(CLK),
-	.rst_n(RESET),
-	.wr(sdram_write),
-	.rd(sdram_read),
-	.cs_sdram(cs_sdram),
-	.abus(sdram_abus),
-	.ben(sdram_ben),
-	.dbus_wr(sdram_dbus_wr),
-	.sdram_dq(sdram_dq),
-	.sdram_cke(sdram_cke),
-	.sdram_cs_n(sdram_cs_n),
-	.sdram_ras_n(sdram_ras_n),
-	.sdram_cas_n(sdram_cas_n),
-	.sdram_we_n(sdram_we_n),
-	.sdram_dqml(sdram_dqml),
-	.sdram_dqmh(sdram_dqmh),
-	.ready_sdram(sdram_ready),
-	.dbus_sdram_rd(dbus_sdram_rd),
-	.sdram_addr(sdram_addr),
-	.sdram_ba(sdram_ba)
-	);
-
 assign	sys_reset =  ~RESET;
 
 
 BranchDecoder	b2v_inst5(
-	.z(Z_ALTERA_SYNTHESIZED),
+	.z(Z),
 	.funct3(INSTRUCTIONS[14:12]),
-	.result(RESULT_ALTERA_SYNTHESIZED),
+	.result(RESULT),
 	.BEQ(beq),
 	.BNE(bne),
 	.BLT(blt),
@@ -388,7 +269,7 @@ MPX2_32BIT	b2v_inst7(
 
 
 CMP8	b2v_inst77(
-	.A(GDFX_TEMP_SIGNAL_5),
+	.A(GDFX_TEMP_SIGNAL_4),
 	.B(_01100111),
 	
 	.E(is_JALR)
@@ -403,8 +284,10 @@ ADD32	b2v_inst8(
 	.OUT(PCplus4));
 
 
-assign	Z = Z_ALTERA_SYNTHESIZED;
-assign	RESULT = RESULT_ALTERA_SYNTHESIZED;
+assign	bus_wr = MemWrite;
+assign	bus_rd = MemRead;
+assign	bus_addr = RESULT;
+assign	bus_wdata = RS2DATA;
 assign	jedan = 1;
 assign	nula = 0;
 
