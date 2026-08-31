4e3a1bd7eee430f1655d9fe6fcb1b978f3e451e5
Neske <agentrambo63@gmail.com>
2026-08-31 16:06:31 +0200

    adde: square test

--- tests/RISCV32I test/CPUTest.v
+++ tests/RISCV32I test/CPUTest.v
@@ -14,36 +14,41 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
-// CREATED		"Mon Aug 31 11:30:09 2026"
+// CREATED		"Mon Aug 31 15:04:36 2026"
 
 module CPUTest(
 	clk,
-	reset,
+	resetBtn0,
 	LED0,
 	LED1
 );
 
 
 input wire	clk;
-input wire	reset;
-output reg	LED0;
-output reg	LED1;
+input wire	resetBtn0;
+output wire	LED0;
+output wire	LED1;
 
 wire	[31:0] addr;
 wire	bus_wr;
 wire	[31:0] data;
 wire	j;
+wire	reset;
 wire	SYNTHESIZED_WIRE_0;
 wire	[31:0] SYNTHESIZED_WIRE_1;
 wire	[31:0] SYNTHESIZED_WIRE_2;
 wire	SYNTHESIZED_WIRE_3;
 wire	SYNTHESIZED_WIRE_4;
-wire	[31:0] SYNTHESIZED_WIRE_5;
+wire	SYNTHESIZED_WIRE_5;
+reg	DFF_inst13;
+reg	DFF_inst9;
 wire	[31:0] SYNTHESIZED_WIRE_6;
-wire	SYNTHESIZED_WIRE_7;
+wire	[31:0] SYNTHESIZED_WIRE_7;
 wire	SYNTHESIZED_WIRE_8;
 wire	SYNTHESIZED_WIRE_9;
+wire	SYNTHESIZED_WIRE_10;
 
+assign	LED0 = DFF_inst9;
 assign	SYNTHESIZED_WIRE_0 = 1;
 
 
@@ -87,48 +92,58 @@ ConstantX	b2v_inst12(
 always@(posedge SYNTHESIZED_WIRE_3)
 begin
 	begin
-	LED1 <= j;
+	DFF_inst13 <= j;
 	end
 end
 
 assign	SYNTHESIZED_WIRE_3 = SYNTHESIZED_WIRE_4 & bus_wr;
 
+assign	LED1 = SYNTHESIZED_WIRE_5 & DFF_inst13;
+
+assign	SYNTHESIZED_WIRE_5 =  ~DFF_inst9;
+
+
+
+RisingEdgeDetector	b2v_inst3(
+	.IN(resetBtn0),
+	.CLK(clk),
+	.OUT(reset));
 
 
 ConstantX	b2v_inst4(
-	.DATA_OUT(SYNTHESIZED_WIRE_5));
+	.DATA_OUT(SYNTHESIZED_WIRE_6));
 	defparam	b2v_inst4.const = 516;
 	defparam	b2v_inst4.size = 32;
 
 
 ConstantX	b2v_inst5(
-	.DATA_OUT(SYNTHESIZED_WIRE_6));
+	.DATA_OUT(SYNTHESIZED_WIRE_7));
 	defparam	b2v_inst5.const = 2147483647;
 	defparam	b2v_inst5.size = 32;
 
 
 CMP32	b2v_inst6(
-	.A(SYNTHESIZED_WIRE_5),
-	.B(addr),
-	
-	.E(SYNTHESIZED_WIRE_7)
-	);
-
-
-CMP32	b2v_inst7(
 	.A(SYNTHESIZED_WIRE_6),
-	.B(data),
+	.B(addr),
 	
 	.E(SYNTHESIZED_WIRE_8)
 	);
 
-assign	SYNTHESIZED_WIRE_9 = SYNTHESIZED_WIRE_7 & SYNTHESIZED_WIRE_8 & bus_wr;
+
+CMP32	b2v_inst7(
+	.A(SYNTHESIZED_WIRE_7),
+	.B(data),
+	
+	.E(SYNTHESIZED_WIRE_9)
+	);
+
+assign	SYNTHESIZED_WIRE_10 = SYNTHESIZED_WIRE_8 & SYNTHESIZED_WIRE_9 & bus_wr;
 
 
-always@(posedge SYNTHESIZED_WIRE_9)
+always@(posedge SYNTHESIZED_WIRE_10)
 begin
 	begin
-	LED0 <= j;
+	DFF_inst9 <= j;
 	end
 end
 
--- tests/RISCV32I test/InstructionMemory version2/riscv32iTest.v
+++ tests/RISCV32I test/InstructionMemory version2/riscv32iTest.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
-// CREATED		"Mon Aug 31 11:49:34 2026"
+// CREATED		"Mon Aug 31 15:04:53 2026"
 
 module riscv32iTest(
 	CLK,
