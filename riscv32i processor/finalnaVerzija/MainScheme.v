b8c8fc1f8e0c2268e28bddd890ccb5b1b7df636e
Neske <agentrambo63@gmail.com>
2026-09-03 14:02:34 +0200

    updated: main scheme, added led periphery and pc display

--- riscv32i processor/finalnaVerzija/MainScheme.v
+++ riscv32i processor/finalnaVerzija/MainScheme.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
-// CREATED		"Fri Aug 28 20:27:59 2026"
+// CREATED		"Thu Sep 03 13:47:45 2026"
 
 module MainScheme(
 	clk,
@@ -30,6 +30,28 @@ module MainScheme(
 	hs,
 	vs,
 	sdram_clk,
+	HEX00,
+	HEX01,
+	HEX02,
+	HEX03,
+	HEX04,
+	HEX05,
+	HEX06,
+	HEX10,
+	HEX11,
+	HEX12,
+	HEX13,
+	HEX14,
+	HEX15,
+	HEX16,
+	HEX20,
+	HEX21,
+	HEX22,
+	HEX23,
+	HEX24,
+	HEX25,
+	HEX26,
+	led,
 	B,
 	G,
 	R,
@@ -52,12 +74,34 @@ output wire	sdram_dqmh;
 output wire	hs;
 output wire	vs;
 output wire	sdram_clk;
+output wire	HEX00;
+output wire	HEX01;
+output wire	HEX02;
+output wire	HEX03;
+output wire	HEX04;
+output wire	HEX05;
+output wire	HEX06;
+output wire	HEX10;
+output wire	HEX11;
+output wire	HEX12;
+output wire	HEX13;
+output wire	HEX14;
+output wire	HEX15;
+output wire	HEX16;
+output wire	HEX20;
+output wire	HEX21;
+output wire	HEX22;
+output wire	HEX23;
+output wire	HEX24;
+output wire	HEX25;
+output wire	HEX26;
+output wire	led;
 output wire	[3:0] B;
 output wire	[3:0] G;
 output wire	[3:0] R;
 output wire	[12:0] sdram_addr;
 output wire	[1:0] sdram_ba;
-output wire	[15:0] sdram_dq;
+inout wire	[15:0] sdram_dq;
 
 wire	[31:0] bus_addr;
 wire	bus_rd;
@@ -65,13 +109,17 @@ wire	[31:0] bus_rdata;
 wire	bus_ready;
 wire	[31:0] bus_wdata;
 wire	bus_wr;
+wire	cs_led;
 wire	cs_lidar;
 wire	cs_sdram;
 wire	cs_vga;
 wire	FB_SEL;
 wire	jedan;
+wire	[31:0] led_rdata;
+wire	led_ready;
 wire	[31:0] lidar_rdata;
 wire	lidar_ready;
+wire	[31:0] pc;
 wire	PIXEL_CHANGED;
 wire	[31:0] sdram_rdata;
 wire	sdram_ready;
@@ -81,6 +129,10 @@ wire	[31:0] vga_rdata;
 wire	vga_ready;
 wire	[9:0] XIN;
 wire	[9:0] YIN;
+wire	SYNTHESIZED_WIRE_0;
+wire	[3:0] SYNTHESIZED_WIRE_1;
+wire	[3:0] SYNTHESIZED_WIRE_2;
+wire	[3:0] SYNTHESIZED_WIRE_3;
 
 assign	sdram_clk = clk;
 wire	[3:0] GDFX_TEMP_SIGNAL_0;
@@ -89,15 +141,16 @@ wire	[3:0] GDFX_TEMP_SIGNAL_0;
 assign	GDFX_TEMP_SIGNAL_0 = {jedan,jedan,jedan,jedan};
 
 
-riscvi32i_4	b2v_inst(
-	.CLK(clk),
-	.RESET(reset),
-	.bus_ready(bus_ready),
-	.bus_rdata(bus_rdata),
+LEDPeripheral	b2v_inst(
+	.clk(clk),
+	.reset(SYNTHESIZED_WIRE_0),
+	.cs(cs_led),
 	.bus_wr(bus_wr),
 	.bus_rd(bus_rd),
-	.bus_addr(bus_addr),
-	.bus_wdata(bus_wdata));
+	.bus_wdata(bus_wdata),
+	.ready(led_ready),
+	.led(led),
+	.bus_rdata(led_rdata));
 
 
 Monitor	b2v_inst1(
@@ -105,13 +158,16 @@ Monitor	b2v_inst1(
 	.SET_PIXEL(SET_PIXEL),
 	.SET_BIT(SET_BIT),
 	.FB_SEL(FB_SEL),
+	.RESET(reset),
 	.XIN(XIN),
 	.YIN(YIN),
+	
 	.hs(hs),
 	.vs(vs),
-	
+	.PIXEL_CHANGED(PIXEL_CHANGED),
 	.B(B),
 	.G(G),
+	
 	.R(R));
 
 
@@ -123,9 +179,18 @@ LidarInterface	b2v_inst10(
 	.lidar_data(lidar_rdata));
 
 
+Binary2BCD	b2v_inst191(
+	.input(pc[7:0]),
+	.bcd_hundreds(SYNTHESIZED_WIRE_3),
+	.bcd_tens(SYNTHESIZED_WIRE_2),
+	.bcd_units(SYNTHESIZED_WIRE_1));
+
+
 VgaBusInterface	b2v_inst2(
 	.cs_vga(cs_vga),
 	.bus_wr(bus_wr),
+	.PIXEL_CHANGED(PIXEL_CHANGED),
+	.clk(clk),
 	.bus_wdata(bus_wdata),
 	.SET_PIXEL(SET_PIXEL),
 	.SET_BIT(SET_BIT),
@@ -134,6 +199,60 @@ VgaBusInterface	b2v_inst2(
 	.vga_rdata(vga_rdata),
 	.XIN(XIN),
 	.YIN(YIN));
+
+
+SevenSegmentInterfaceDEC	b2v_inst20(
+	
+	.en(jedan),
+	.x(SYNTHESIZED_WIRE_1),
+	.a(HEX00),
+	.b(HEX01),
+	.c(HEX02),
+	.d(HEX03),
+	.e(HEX04),
+	.f(HEX05),
+	.g(HEX06)
+	);
+
+
+SevenSegmentInterfaceDEC	b2v_inst21(
+	
+	.en(jedan),
+	.x(SYNTHESIZED_WIRE_2),
+	.a(HEX10),
+	.b(HEX11),
+	.c(HEX12),
+	.d(HEX13),
+	.e(HEX14),
+	.f(HEX15),
+	.g(HEX16)
+	);
+
+
+SevenSegmentInterfaceDEC	b2v_inst22(
+	
+	.en(jedan),
+	.x(SYNTHESIZED_WIRE_3),
+	.a(HEX20),
+	.b(HEX21),
+	.c(HEX22),
+	.d(HEX23),
+	.e(HEX24),
+	.f(HEX25),
+	.g(HEX26)
+	);
+
+
+riscv32iTest	b2v_inst3(
+	.CLK(clk),
+	.RESET(reset),
+	.bus_ready(bus_ready),
+	.bus_rdata(bus_rdata),
+	.bus_wr(bus_wr),
+	.bus_rd(bus_rd),
+	.bus_addr(bus_addr),
+	.bus_wdata(bus_wdata),
+	.PC(pc));
 
 
 sdram_wrapper	b2v_inst4(
@@ -160,20 +279,25 @@ sdram_wrapper	b2v_inst4(
 	);
 
 
+assign	SYNTHESIZED_WIRE_0 =  ~reset;
 
-BusDecoder	b2v_inst9(
+
+BusDecoder	b2v_inst7(
 	.bus_wr(bus_wr),
 	.bus_rd(bus_rd),
 	.sdram_ready(sdram_ready),
 	.lidar_ready(lidar_ready),
 	.vga_ready(vga_ready),
+	.led_ready(led_ready),
 	.bus_addr(bus_addr),
+	.led_rdata(led_rdata),
 	.lidar_rdata(lidar_rdata),
 	.sdram_rdata(sdram_rdata),
 	.vga_rdata(vga_rdata),
 	.cs_sdram(cs_sdram),
 	
 	.cs_vga(cs_vga),
+	.cs_led(cs_led),
 	.bus_ready(bus_ready),
 	.bus_rdata(bus_rdata));
 
