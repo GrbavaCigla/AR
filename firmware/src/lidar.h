5d0b4f34d43d4f10c1ac805c3885ca45c61cef88
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-09-06 00:22:47 +0200

    feat: final code yay

--- firmware/src/lidar.h
+++ firmware/src/lidar.h
@@ -3,10 +3,7 @@
 
 #define LIDAR_DWORD MMIO_REG(LIDAR_DWORD_ADDRESS)
 
-// Flags
-#define LIDAR_READY_BIT ((dword_t)1 << 31)
-
-// Helpers
-#define LIDAR_IS_READY(word) ((bool_t)(((word) & LIDAR_READY_BIT) != 0))
-#define LIDAR_GET_X(word) ((word_t)((word) & 0x3FFu))
-#define LIDAR_GET_Y(word) ((word_t)(((word) >> 10) & 0x3FFu))
+#define LIDAR_IS_READY(word) ((bool_t)((word) >> 31))
+#define LIDAR_GET_X(word) ((word_t)(((word) >> 10) & 0x3FFu))
+#define LIDAR_GET_Y(word) ((word_t)((word) & 0x3FFu))
+#define LIDAR_GET_N(word) ((word_t)(((word) >> 20) & 0x3FFu))
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,19 +1,52 @@
 #include "common.h"
-#include "vga.h"
 #include "lidar.h"
+#include "vga.h"
+
+#define BUFFERING 1
+
+void vga_clear()
+{
+    for (register dword_t x = 0; x < 800; x++) {
+        for (register dword_t y = 0; y < 600; y++) {
+            VGA_WRITE_PIXEL(x, y, 0);
+        }
+    }
+}
 
 void main(void)
 {
-    register dword_t i = 0, lidar;
+    vga_clear();
+    VGA_SWAP();
+    vga_clear();
+
+    register dword_t counter = 0, last_n = 0xFFFF;
+    register dword_t x, y, n;
     while (1) {
-        lidar = LIDAR_DWORD;
+        register dword_t word = LIDAR_DWORD;
 
-        VGA_WRITE_PIXEL(LIDAR_GET_X(lidar), LIDAR_GET_Y(lidar), 1);
+        if (!LIDAR_IS_READY(word))
+            continue;
 
-        i++;
-        if (i > 24) {
-            i = 0;
+        n = LIDAR_GET_N(word);
+        if (last_n == n)
+            continue;
+
+        last_n = n;
+
+        counter++;
+        x = LIDAR_GET_X(word);
+        y = LIDAR_GET_Y(word);
+        VGA_WRITE_PIXEL(x, y, 1);
+
+#if BUFFERING == 1
+        if (counter >= 2000) {
             VGA_SWAP();
+            vga_clear();
+#elif BUFFERING == 0
+        if (counter >= 4000) {
+            VGA_SWAP();
+#endif
+            counter = 0;
         }
     }
 }
